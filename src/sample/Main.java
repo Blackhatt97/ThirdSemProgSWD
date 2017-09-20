8bf8dc2b5cda993149c523c8e98d891de8dc0773
CIA <CIA@CIA.CIA>
2017-09-20 11:49:39 +0200

    testo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -27,5 +27,5 @@ public class Main extends Application {
 
     }
 
-    //testcommentz
+    //testcommentzssss
 }
--- src/sample/Model/MovieData.java
+++ src/sample/Model/MovieData.java
@@ -15,20 +15,5 @@ public class MovieData {
     public ObservableList<Movie> getMovieList() {
         return movieList;
     }
-<<<<<<< HEAD
 
-    public void loadList() {
-        DBConn dbConn = new DBConn();
-        //movieList = dbConn.getAllMovies();
-        dbConn = null;
-
-    }
-=======
-//
-//    public void loadList() {
-//        DBConn dbConn = new DBConn();
-//        movieList = dbConn.getAllMovies();
-//        dbConn = null;
-//    }
->>>>>>> 098e495a919fdd58c5088c56f5292db32f96fe6b
 }
